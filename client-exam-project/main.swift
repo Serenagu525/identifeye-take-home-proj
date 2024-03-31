6f8f09f1cb94061b316be0904c24b5c6fc3f4743
Serenagu525 <41308432+Serenagu525@users.noreply.github.com>
2024-03-31 13:05:12 -0700

    initial implementation

--- client-exam-project/main.swift
+++ client-exam-project/main.swift
@@ -2,10 +2,112 @@
 //  main.swift
 //  client-exam-project
 //
-//  Created by Yuxuan Gu on 3/25/24.
+//  Created by Yuxuan Gu on 3/31/24.
 //
 
 import Foundation
 
-print("Hello, World!")
+struct Patient {
+    let name: String
+    var numExam: Int
+    mutating func incrementExam() {
+        numExam += 1
+    }
+    mutating func decrementExam() {
+        numExam -= 1
+    }
+}
+
+
+var patients = [String: Patient]()
+var exams = [String: String]()
+var orderedPatients = [String]()  //used to maintain insertion order
+
+
+func aggregateData(from filename: String) {
+    do {
+        let contents = try String(contentsOfFile: filename)
+        let lines = contents.split(separator:"\n")
+        for instruction in lines {
+            let components = instruction.components(separatedBy: .whitespaces)
+                if components.isEmpty { continue }
+            switch components[1] {
+            case "PATIENT":
+                if components[0] == "ADD" {
+                    addPatient(inst: components)
+                } else if components[0] == "DEL" {
+                    deletePatient(inst: components)
+                }
+            case "EXAM":
+                if components[0] == "ADD" {
+                    addExam(inst: components)
+                } else if components[0] == "DEL" {
+                    deletePatient(inst: components)
+                }
+            default:
+                break
+            }
+        }
+        displaySummary()
+    } catch {
+        print("Error reading file: \(error)")
+    }
+    
+}
+func addPatient(inst: [String]) {
+    let patientIdentifier = inst[2]
+    let patientName = inst[3...].joined(separator: " ")
+    //check if identifier already exists
+    if patients.keys.contains(patientIdentifier) {
+        return
+    }
+    patients[patientIdentifier] = Patient(name: patientName, numExam: 0)
+    orderedPatients.append(patientIdentifier)
+}
+
+func addExam(inst:[String]) {
+    let patientIdentifier = inst[2]
+    let examIdentifier = inst[3]
+    //check if patient identifier exists
+    if !patients.keys.contains(patientIdentifier) {
+        return
+    }
+    //check if exam identifier exists
+    if exams.keys.contains(examIdentifier) {
+        return
+    }
+    patients[patientIdentifier]!.incrementExam()
+    exams[examIdentifier] = patientIdentifier
+}
+
+func deleteExam(inst: [String]) {
+    let examIdentifier = inst[2]
+    //check if exam identifier exists
+    if !exams.keys.contains(examIdentifier) {
+        return
+    }
+    patients[exams[examIdentifier]!]!.decrementExam()
+    exams.removeValue(forKey: examIdentifier)
+}
+
+func deletePatient(inst: [String]) {
+    let patientIdentifier = inst[2]
+    if !patients.keys.contains(patientIdentifier) {
+        return
+    }
+    exams = exams.filter {$0.value != patientIdentifier}
+    patients.removeValue(forKey: patientIdentifier)
+    orderedPatients.removeAll {$0 == patientIdentifier}
+}
+
+func displaySummary() {
+    for id in orderedPatients {
+        if let patientInfo = patients[id] {
+            print("Name: \(patientInfo.name), Id: \(id), Exam Count: \(patientInfo.numExam)")
+        }
+    }
+}
+let filename = "/Users/yuxuangu/Desktop/swift/identifeye HEALTH take home proj/example-input.txt"
+aggregateData(from: filename)
+
 
